11c857f4f0d9f7b52786a765815c2b47d33bf3b1
Masedi Ncamani <masedi.ncamani@gmail.com>
2024-01-14 10:43:49 +0200

    Worked on Search Engine.
    Still a few bugs.

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,56 @@
 import React, { useState } from "react";
-import "./Weather.css";
-import "./App.css";
-import axios from "axios";
-import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      temperature: response.data.temperature.current,
-      wind: response.data.wind.speed,
-      city: response.data.city,
-      humidity: response.data.temperature.humidity,
-      description: response.data.condition.description,
+      temperature: response.data.main.temperature,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
       iconUrl:
         "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png",
-      date: new Date(response.data.time * 1000),
+      wind: response.data.wind.speed,
+      city: response.data.name,
     });
   }
+
+  function search() {
+    const apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row ">
             <div className="col-9">
               <input
+                onChange={handleCityChange}
                 type="search"
                 placeholder="Enter a city..."
                 className="form-control"
                 autoFocus="on"
               />
             </div>
-            <div className="col-5">
+            <div className="col-3">
               <input
                 type="submit"
                 value="search"
@@ -43,8 +59,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <WeatherInfo />
+        <WeatherInfo data={weatherData} />
       </div>
     );
+  } else {
+    search();
+    return "Loading...";
   }
 }
