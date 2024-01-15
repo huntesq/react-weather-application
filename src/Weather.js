30d6c99b96c3c552ad49134e24db9d9c6b1f2567
Masedi Ncamani <masedi.ncamani@gmail.com>
2024-01-15 09:31:25 +0200

    Integrated Weather Icon.

--- src/Weather.js
+++ src/Weather.js
@@ -14,8 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl:
-        "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
