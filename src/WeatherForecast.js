c1b6e7c199e39c00eec571723ee78b74dccafe39
Masedi Ncamani <masedi.ncamani@gmail.com>
2024-01-17 12:17:36 +0200

    Integrated useEffect to run new code after component has loaded, if something has changed.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setLoaded(true);
     setForecast(response.data.daily);
